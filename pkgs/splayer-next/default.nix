cf6ce6aa67647dc6388874f7a9e8b231931efd18
KyvYang <kyvyang@foxmail.com>
2026-07-28 14:13:56 +0800

    更新

--- pkgs/splayer-next/default.nix
+++ pkgs/splayer-next/default.nix
@@ -33,9 +33,9 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "SPlayer-Dev";
     repo = "SPlayer-Next";
-    rev = "67145444ff26fecdf2363f4aa9eb34878d8b5fac"; # No releases yet
+    rev = "5139937613ce5c472107b81dc819b8ad33d6e364"; # No releases yet
     fetchSubmodules = false;
-    hash = "sha256-REL8lQ7EFZkG+X/i2Y9nlkdtZsGF8TWKthHGnr4eJOs=";
+    hash = "sha256-A/U14b0HzZ0ZUvco0TQAQJ/bJti3thsXfyR5uwct/W0=";
   };
 
   pnpmDeps = fetchPnpmDeps {
